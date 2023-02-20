1f7475be0890cc8c06b24853051c332d8fcc61bd
Piotr <piotr.wirth@onet.pl>
2023-02-20 16:43:45 +0100

    Added and migrated Profile model

---
+++ Photo/accounts/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.conf import settings
+
+# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    tier = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.user.username
