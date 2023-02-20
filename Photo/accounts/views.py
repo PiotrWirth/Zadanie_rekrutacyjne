6469a2930f81eb168012f1bfd284121d21c3369a
Piotr <piotr.wirth@onet.pl>
2023-02-20 16:52:13 +0100

    Prepared forms and views for accounts app

--- Photo/accounts/views.py
+++ Photo/accounts/views.py
@@ -1,3 +1,32 @@
+from django.http import HttpResponse
 from django.shortcuts import render
-
+from .forms import LoginForm
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from picture.models import Picture
 # Create your views here.
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            user = authenticate(
+                request, username=data['username'], password=data['password'])
+            if user is not None:
+                login(request, user)
+                return HttpResponse('User logged in')
+            else:
+                return HttpResponse('invalid credentials')
+
+    else:
+        form = LoginForm()
+    return render(request, 'accounts/login.html', {'form': form})
+
+@login_required
+def index(request):
+    current_user = request.user
+    pictures = Picture.objects.filter(user=current_user)
+    return render(request, 'index.html', {'pictures': pictures})
+
