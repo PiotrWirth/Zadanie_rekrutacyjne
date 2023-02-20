660335e6ed6935d724c765f0f1151d766ab7e3c6
Piotr <piotr.wirth@onet.pl>
2023-02-20 17:39:57 +0100

    Created forms and and updated views for picture app

--- Photo/picture/views.py
+++ Photo/picture/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UploadPictureForm
+from django.contrib.auth.decorators import login_required
+from accounts.models import Profile
+from .models import Picture
 
 # Create your views here.
+
+@login_required
+def picture_upload(request):
+    if request.method == 'POST':
+        form = UploadPictureForm(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            new_item = form.save(commit=False)
+            new_item.user = request.user
+            new_item.save()
+            return redirect('picture:tier')
+    else:
+        form = UploadPictureForm(data=request.GET)
+    return render(request, 'picture/upload.html',{'form':form})
+
+def tier(request):
+    current_user = request.user
+    account = Profile.objects.get(user=current_user)
+    photo = Picture.objects.filter(user=current_user).order_by('-id')[0]
+    return render(request, 'picture/tier.html',{'account':account, 'photo':photo})
