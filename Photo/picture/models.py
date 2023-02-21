466d1844be8e9cb98fd8d62fc729b46026c73f0b
Piotr <piotr.wirth@onet.pl>
2023-02-21 17:18:44 +0100

    Modiefied and migrated Profile and Picture models

--- Photo/picture/models.py
+++ Photo/picture/models.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from imagekit.models import ImageSpecField
 from imagekit.processors import ResizeToFill
 
+
 # Create your models here.
 
 class Picture(models.Model):
@@ -10,5 +11,6 @@ class Picture(models.Model):
     image = models.ImageField(upload_to='images')
     thumbnail200x200 = ImageSpecField(source='image', processors=[ResizeToFill(200, 200)], format="PNG", options={'quality': 60})
     thumbnail400x400 = ImageSpecField(source='image', processors=[ResizeToFill(400, 400)], format="PNG", options={'quality': 60})
+    created_at = models.TimeField(auto_now=True)
     
     
